81d43633cfbf5b3bd2a8836d94a1425dd5cc3dda
admin <admin@DESKTOP-G9DURCR>
2024-09-10 16:37:16 +0530

    Initial Commit

---
+++ practiceTestNG/src/test/java/test/Errormessage.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Errormessage {
+	
+	@Test
+	public void enterwrongpassword() throws InterruptedException {
+		WebDriverManager.firefoxdriver().setup();
+		FirefoxDriver driver=new FirefoxDriver();
+		Thread.sleep(1000);
+		driver.get("https://app.germanyiscalling.com/common/login/?next=https%3A%2F%2Fapp.germanyiscalling.com%2Fcv%2Fhome%2F");
+		Thread.sleep(1000);
+		driver.findElement(By.id("username")).sendKeys("llikitha364@gmail.com");
+		Thread.sleep(1000);
+		driver.findElement(By.id("password")).sendKeys("Liki1");
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector("button[type='submit']")).click();
+		Thread.sleep(1000);
+		driver.close();
+		
+	}
+
+}
